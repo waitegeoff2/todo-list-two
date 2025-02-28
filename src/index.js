dded27afc1b23901d4624bfe7b26ed02a0a08275
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-02-28 13:08:00 -0500

    Add logic to switch between projects

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import "./styles.css";
 import { toDoItem } from "./to-do-items";
 import { Project } from "./project";
 
+//images
+import trashImg from "./images/trash-can-outline.svg";
+import checkImg from "./images/check-outline.svg";
+
 const toDoHeader = document.querySelector(".toDoSection");
 const toDoList = document.querySelector(".toDoList");
 const toDoLabels = document.querySelector(".toDoLabels");
@@ -19,17 +23,198 @@ const currentProject = document.querySelector(".projectTitle");
 // for storing each project
 let projectList = [];
 
+//create a project and add to project array
+
+function createProject(name) {
+    let newProject = new Project(name);
+    projectList.push(newProject);
+    return newProject;
+}
+
 let defaultProject = new Project("default");
 
-defaultProject.addToDo("Laundry", "do it", "sept", "high");
+let thisProject = defaultProject;
 
 console.log(defaultProject);
 
+//displaying a project on the to-do page
+
+function displayProject() {
+    toDoList.innerHTML = ""; 
+    
+    console.log(defaultProject);
+
+    function appendToList(arrayItem) {
+        console.log(defaultProject.list.indexOf(arrayItem));
+
+
+        const itemContainer = document.createElement("div");
+        itemContainer.classList.add("itemContainer");
+        toDoList.appendChild(itemContainer);
+
+        const leftDiv = document.createElement("div");
+        leftDiv.classList.add("leftDiv");
+        itemContainer.appendChild(leftDiv);
+
+        //title
+
+        const listItemTitle = document.createElement("h1");
+        listItemTitle.classList.add("listItemTitle");
+        listItemTitle.textContent = arrayItem.title;
+        leftDiv.appendChild(listItemTitle);
+
+        //due date
+
+        const listItemDate = document.createElement("span");
+        listItemDate.classList.add("listItemDate");
+        listItemDate.textContent = "Due date:" + arrayItem.dueDate;
+        leftDiv.appendChild(listItemDate);
+
+        //right div
+        const rightDiv = document.createElement("div");
+        rightDiv.classList.add("rightDiv");
+        itemContainer.appendChild(rightDiv);
+
+        //add delete button
+
+        const deleteImage = document.createElement("img");
+        deleteImage.src = trashImg;
+        deleteImage.classList.add("deleteImage");
+        rightDiv.appendChild(deleteImage);
+
+        //make delete functional
+        deleteImage.addEventListener("click", () => {
+            thisProject.list.splice(defaultProject.list.indexOf(arrayItem), 1);
+            displayProject();
+        })
+
+        //add finished button
+        const checkImage = document.createElement("img");
+        checkImage.src = checkImg;
+        checkImage.classList.add("checkImage");
+        rightDiv.appendChild(checkImage);
+
+        //make finished functional
+
+        checkImage.addEventListener("click", () => {
+            if (listItemDate.classList.contains("completedTask") && listItemTitle.classList.contains("completedTask")) {
+                listItemDate.classList.remove("completedTask");
+                listItemTitle.classList.remove("completedTask");
+            } else {
+                listItemDate.classList.add("completedTask");
+                listItemTitle.classList.add("completedTask");
+            };
+        })
+
+
+    }
+
+    thisProject.list.forEach(appendToList);
+
+    //project(HTLM NAME).list.forEAch
+}
+
 // DOM STUFF
+
+//to do buttons
+
+//bring up modal 
 
 addToDoButton.addEventListener("click", () => {
     addToDoDialog.showModal();
 })
 
+//close modal
 
+closeToDoDialogBtn.addEventListener("click", () => {
+    addToDoDialog.close();
+    toDoForm.reset();
+})
+
+//submit to do form
+
+toDoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    addToDoDialog.close();
+
+    const itemTitle = document.querySelector("#toDoTitle");
+    const itemDescription = document.querySelector("#toDoDescription");
+    const itemDue = document.querySelector("#toDoDueDate");
+    const toDoPriority = document.querySelector("#toDoPriority");
+
+    let title = itemTitle.value;
+    let description = itemDescription.value;
+    let dueDate = itemDue.value;
+    let priority = toDoPriority.value;
+
+    thisProject.addToDo(title, description, dueDate, priority);
+
+    console.log(thisProject);
+
+    toDoForm.reset();
+
+    displayProject();
+
+
+})
+
+//NEW PROJECT
+
+//new project btn
+newProjectButton.addEventListener("click", () => {
+    newProjectDialog.showModal();
+})
+
+// close project modal
+closeProjectDialogBtn.addEventListener("click", () => {
+    newProjectDialog.close();
+    newProjectForm.reset();
+})
+
+// submit modal to create new project
+
+newProjectForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    //close modal
+
+    newProjectDialog.close();
+
+    //create new project object
+
+    const projectName = document.querySelector("#projTitle");
+
+    //create a new project with the name of what you just entered
+   
+    let nextProject = new Project(projectName.value);
+
+    //make current project equal what you entered in form
+
+    thisProject = nextProject;
+
+    console.log(thisProject);
+
+    // remove default DOM
+
+    toDoHeader.removeChild;
+    
+    toDoList.innerHTML = "";
+
+    //add Header to page
+
+    const projectTitle = document.createElement("h3");
+    projectTitle.classList.add("projectTitle");
+    projectTitle.textContent = projectName.value;
+    toDoLabels.appendChild(projectTitle);
+
+    //add sidebar ITEM
+
+    console.log(nextProject);
+    
+})
+
+//BUTTONS
+
+//project button
 
